808a79c95ec7328b7a822bebe4a23df63ef903d6
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2017-10-17 09:13:43 +0200

    fixed minor layout problems

--- app/src/main/java/com/example/miszk/rozpoznawarkatablic/MainActivity.java
+++ app/src/main/java/com/example/miszk/rozpoznawarkatablic/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         txtView=(TextView)findViewById(R.id.tv_answer);
         txtView.setText(message);
 
